ef373dab53eee9ad33fbddfc07eff816b2b61302
aziyatdin0v <azat.ziyatdinov@yandex.ru>
2022-10-26 20:40:06 +0300

    first commit

---
+++ src/test/java/guru/qa/SelenideTest.java
@@ -0,0 +1,51 @@
+package guru.qa;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    @DisplayName("Проверить, что страница SoftAssertions->Enterprise содержит надпись 'Build like the best'")
+    public void checkPageLabel() {
+        open("https://github.com");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("h1").shouldHave(text("Build like the best"));
+
+    }
+
+    @Test
+    @DisplayName("Проверить работу метода drag and drop")
+    public void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+
+    @Test
+    @DisplayName("Проверить работу метода drag and drop через actions") //не работает
+    public void dragAndDropTestdragAndDropWithActionTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
